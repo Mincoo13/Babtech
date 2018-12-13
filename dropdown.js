9de509e7fd4ae8a303c6fdcb1fd2a2b4b5e06dd6
Milos Medo <milos.medo2@gmail.com>
2018-12-13 12:31:47 +0100

    prvy commit

---
+++ dropdown.js
@@ -0,0 +1,64 @@
+var menu_items = {
+    "items": [
+        {
+            "id": "1",
+            "name": "Domov",
+            "src": "index.html"
+        },
+        {
+            "id": "2",
+            "name": "Novinky",
+            "src": "news.html"
+        },
+        {
+            "id": "3",
+            "name": "Autori",
+            "src": "#"
+        },
+        {
+            "id": "1.1",
+            "name": "Dominik Janecký",
+            "src": "#"
+        },
+        {
+            "id": "1.2",
+            "name": "Lea Lapšanská",
+            "src": "#"
+        },
+        {
+            "id": "1.3",
+            "name": "Miloš Medo",
+            "src": "#"
+        },
+        {
+            "id": "1.1.1",
+            "name": "O mne",
+            "src": "dominik_about.html"
+        },
+        {
+            "id": "1.1.2",
+            "name": "Moja hra",
+            "src": "dominik_game.html"
+        },
+        {
+            "id": "1.2.1",
+            "name": "O mne",
+            "src": "lea_about.html"
+        },
+        {
+            "id": "1.2.2",
+            "name": "Moja hra",
+            "src": "lea_game.html"
+        },
+        {
+            "id": "1.3.1",
+            "name": "O mne",
+            "src": "milos_about.html"
+        },
+        {
+            "id": "1.3.2",
+            "name": "O mne",
+            "src": "milos_game.html"
+        }
+    ]
+}

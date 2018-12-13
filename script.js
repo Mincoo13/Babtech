9b7f979fd3a3d902b9ed43624885dab34abfccd8
Milos Medo <milos.medo2@gmail.com>
2018-12-13 15:17:01 +0100

    dokonceny druhy level dropdownu

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ for (var i = 0; i < menu_items.items.length; i++) {
         menu.appendChild(li[i]);
         li[i].appendChild(item[i]);
         if(menu_items.items[i].subitems != null) { 
+            li[i].setAttribute("class", "nav-item dropdown");
             item[i].setAttribute("class", "nav-link dropdown-toggle");
             item[i].setAttribute("id", "navbarDropdown");
             item[i].setAttribute("role", "button");
@@ -27,11 +28,13 @@ for (var i = 0; i < menu_items.items.length; i++) {
             sec_level[i].setAttribute("id","dropdown");
             sec_level[i].setAttribute("class","dropdown-menu");
             sec_level[i].setAttribute("aria-labelledby","navbarDrodown");
+            li[i].appendChild(sec_level[i]);
             for(var j=0;j<menu_items.items[i].subitems.length; j++){
                 sec_a[j]= document.createElement("a");
                 sec_a[j].setAttribute("class","dropdown-item");
                 sec_a[j].innerHTML=menu_items.items[i].subitems[j].name;
-                sec_a[j].href = menu_items.items[i].subitems[j].src;
+                sec_a[j].href = menu_items.items[i].subitems[j].subitems[1].src;
+                sec_level[i].appendChild(sec_a[j]);
                 console.log("som tu heh");
             }
             

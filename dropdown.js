44b5806727eb1f76426187c5bd57313e9a3ad480
Milos Medo <milos.medo2@gmail.com>
2018-12-13 14:59:47 +0100

    druhy commit

--- dropdown.js
+++ dropdown.js
@@ -1,4 +1,4 @@
-var menu_items = {
+var menu_items = `{
     "items": [
         {
             "id": "1",
@@ -13,52 +13,62 @@ var menu_items = {
         {
             "id": "3",
             "name": "Autori",
-            "src": "#"
-        },
-        {
-            "id": "1.1",
-            "name": "Dominik Janecký",
-            "src": "#"
-        },
-        {
-            "id": "1.2",
-            "name": "Lea Lapšanská",
-            "src": "#"
-        },
-        {
-            "id": "1.3",
-            "name": "Miloš Medo",
-            "src": "#"
-        },
-        {
-            "id": "1.1.1",
-            "name": "O mne",
-            "src": "dominik_about.html"
-        },
-        {
-            "id": "1.1.2",
-            "name": "Moja hra",
-            "src": "dominik_game.html"
-        },
-        {
-            "id": "1.2.1",
-            "name": "O mne",
-            "src": "lea_about.html"
-        },
-        {
-            "id": "1.2.2",
-            "name": "Moja hra",
-            "src": "lea_game.html"
-        },
-        {
-            "id": "1.3.1",
-            "name": "O mne",
-            "src": "milos_about.html"
-        },
-        {
-            "id": "1.3.2",
-            "name": "O mne",
-            "src": "milos_game.html"
+            "src": "#",
+            "subitems":[
+                {
+                    "id": "3.1",
+                    "name": "Dominik Janecký",
+                    "src": "#",
+                    "subitems":[
+                        {
+                            "id": "3.1.1",
+                            "name": "O mne",
+                            "src": "dominik_about.html"
+                        },
+                        {
+                            "id": "3.1.2",
+                            "name": "Moja hra",
+                            "src": "dominik_game.html"
+                        }
+                    ]
+                },
+                {
+                    "id": "3.2",
+                    "name": "Lea Lapšanská",
+                    "src": "#",
+                    "subitems":[
+                        {
+                            "id": "3.2.1",
+                            "name": "O mne",
+                            "src": "lea_about.html"
+                        },
+                        {
+                            "id": "3.2.2",
+                            "name": "Moja hra",
+                            "src": "lea_game.html"
+                        }
+                    ]
+                },
+                {
+                    "id": "3.3",
+                    "name": "Miloš Medo",
+                    "src": "#",
+                    "subitems":[
+                        {
+                            "id": "3.3.1",
+                            "name": "O mne",
+                            "src": "milos_about.html"
+                        },
+                        {
+                            "id": "3.3.2",
+                            "name": "O mne",
+                            "src": "milos_game.html"
+                        }
+                    ]
+                }
+
+            ]
         }
+        
     ]
-}
+}`
---
+++ script.js
@@ -0,0 +1,40 @@
+var menu_items = JSON.parse(menu_items);
+var item = [];
+var li = [];
+var sec_level = [];
+var sec_a = [];
+var menu = document.getElementById("menu");
+
+
+for (var i = 0; i < menu_items.items.length; i++) {
+        li[i] = document.createElement("li");
+        li[i].setAttribute("class", "nav-item");
+        item[i] = document.createElement("a");
+        item[i].innerHTML = menu_items.items[i].name;
+        item[i].href = menu_items.items[i].src;
+        
+        console.log(i, menu_items.items[i], item[i]);
+        menu.appendChild(li[i]);
+        li[i].appendChild(item[i]);
+        if(menu_items.items[i].subitems != null) { 
+            item[i].setAttribute("class", "nav-link dropdown-toggle");
+            item[i].setAttribute("id", "navbarDropdown");
+            item[i].setAttribute("role", "button");
+            item[i].setAttribute("data-toggle", "dropdown");
+            item[i].setAttribute("aria-haspopup", "true");
+            item[i].setAttribute("aria-expanded", "false");
+            sec_level[i]= document.createElement("div");
+            sec_level[i].setAttribute("id","dropdown");
+            sec_level[i].setAttribute("class","dropdown-menu");
+            sec_level[i].setAttribute("aria-labelledby","navbarDrodown");
+            for(var j=0;j<menu_items.items[i].subitems.length; j++){
+                sec_a[j]= document.createElement("a");
+                sec_a[j].setAttribute("class","dropdown-item");
+                sec_a[j].innerHTML=menu_items.items[i].subitems[j].name;
+                sec_a[j].href = menu_items.items[i].subitems[j].src;
+                console.log("som tu heh");
+            }
+            
+        }
+        else item[i].setAttribute("class", "nav-link");
+}
